e7efc7aabf012be435c68e9cbe705e3c2d2f7cf0
Cesar Rodriguez <cesar@dealdash.com>
2016-02-05 19:22:38 +0100

    driver app

--- index.ios.js
+++ index.ios.js
@@ -1,7 +1,7 @@
 /**
  * DealExplorer
  *
- * @providesModule Game2048
+ * @providesModule DealExplorer
  * @flow
  */
 'use strict';
@@ -13,43 +13,125 @@ var {
 	StyleSheet,
 	View,
 	Image,
+	Text,
+	TouchableHighlight,
 	} = React;
 
-var Board = require('Board');
-var BoardData = require('BoardData');
-var GameEndOverlay = require('GameEndOverlay');
-
 var DealExplorer = React.createClass({
+	watchID: (null: ?number),
+	token: "",
 
 	getInitialState() {
-		return { board: new BoardData() }
+		return {
+			lat: 'unknown',
+			lon: 'unknown',
+			token: ''
+		};
 	},
 
-	restartGame() {
-		this.setState({ board: new BoardData() });
+	componentDidMount: function() {
+		navigator.geolocation.getCurrentPosition(
+			(position) => {
+			},
+			(error) => alert(error.message),
+			{enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+		);
+		this.watchID = navigator.geolocation.watchPosition((position) => {
+			var lat = JSON.stringify(position["coords"]["latitude"]);
+			var lon = JSON.stringify(position["coords"]["longitude"]);
+			this.setState({lat});
+			this.setState({lon});
+		});
+	},
+
+	componentWillUnmount: function() {
+		navigator.geolocation.clearWatch(this.watchID);
 	},
 
 	render: function() {
-
 		return (
-			<View
-				style={styles.container}>
-				<Image source={require('./images/logo.png')} style={styles.logo} />
-				<Board data={this.state.board}></Board>
-				<GameEndOverlay data={this.state.board} onRestart={() => this.restartGame()} />
+			<View>
+				<Text style={styles.logo}>
+					LAT: {this.state.lat}, LON: {this.state.lon}
+				</Text>
+				<View style={styles.container}>
+					<TouchableHighlight onPress={this._login} style={styles.button}>
+						<Text>LOGIN</Text>
+					</TouchableHighlight>
+
+					<TouchableHighlight onPress={this._updateLocation} style={styles.button}>
+						<Text>UPDATE LOCATION</Text>
+					</TouchableHighlight>
+				</View>
 			</View>
 		);
-	}
+	},
+
+	_login: function () {
+	fetch("http://192.168.1.91:8000/login/password", {
+		method : "POST",
+		body   : JSON.stringify({"username": "admin", "password": "admin"}),
+		headers: {
+			'Accept'      : 'application/json',
+			'Content-Type': 'application/json'
+		}
+	})
+		.then((response) => response.json())
+		.then((responseData) => {
+			var token = responseData['auth_token'];
+			this.setState({token});
+			alert('logged!');
+		})
+		.done();
+	},
+
+	_updateLocation: function () {
+		navigator.geolocation.getCurrentPosition(
+			(position) => {
+				var lat = JSON.stringify(position["coords"]["latitude"]);
+				var lon = JSON.stringify(position["coords"]["longitude"]);
+				this.setState({lat});
+				this.setState({lon});
+				fetch("http://192.168.1.91:8000/deal/0339788D-961C-42B5-AE7C-1948BE417E29/update-coords", {
+					method : "POST",
+					body   : JSON.stringify({"lat": lat, "lon": lon}),
+					headers: {
+						'Accept'      : 'application/json',
+						'Content-Type': 'application/json',
+						'X-Auth-Token': this.state.token
+					}
+				})
+					.then((response) => response.json())
+					.then((responseData) => {
+						alert(responseData['success']);
+					})
+					.done();
+			},
+			(error) => alert(error.message),
+			{enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+		);
+	},
 });
 
 var styles = StyleSheet.create({
-	container: {
-		flex: 1,
+	logo: {
 		justifyContent: 'center',
 		alignItems: 'center',
+		flex: 1,
+		margin: 50,
+		fontWeight: '500'
 	},
-	logo: {
-		bottom: 30,
+	container: {
+		flex          : 1,
+		flexDirection : 'row',
+		justifyContent: 'center',
+		alignItems    : 'center'
+	},
+	button   : {
+		backgroundColor: '#EEEEEE',
+		padding        : 10,
+		marginRight    : 5,
+		marginLeft     : 5
 	}
 });
 
